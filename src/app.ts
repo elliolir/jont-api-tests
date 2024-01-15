be13f8656dd43ea5c34ca5ea9af9e34752ac7ce1
Dzmitry Bartalevich <dz.bartalevich@gmail.com>
2024-01-15 16:42:45 +0100

    add basic Todo API

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import "reflect-metadata";
+import bodyParser from 'body-parser';
 import express from 'express';
 
 import {todosRouter} from "@/routes/todos/todos.route";
 
 export const app = express();
+
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.use(todosRouter);
--- src/routes/todos/todos.route.ts
+++ src/routes/todos/todos.route.ts
@@ -1,7 +1,40 @@
 import { Router } from 'express';
 
+import {Todo} from '@/entities/Todo';
+import dataSource from "@/data-source";
+
+const todoRepository = dataSource.getRepository(Todo);
+
 export const todosRouter = Router();
 
-todosRouter.get('/todos', (req, res) => {
-  res.status(200).send({id: 1, value: 'hello'})
+todosRouter.get('/todos', async (req, res) => {
+  const todos = await todoRepository.find();
+  res.status(200).send(todos)
+})
+
+todosRouter.get('/todos/:id', async (req, res) => {
+  const id = +req.params.id;
+  const todo = await todoRepository.findOneByOrFail({id});
+
+  res.status(200).send(todo);
+})
+
+todosRouter.post('/todos', async (req, res) => {
+  const {value} = req.body;
+  const todo = await todoRepository.create({value});
+  const savedTodo = await todoRepository.save(todo);
+
+  res.status(200).send(savedTodo);
+})
+
+todosRouter.patch('/todos/:id', async (req, res) => {
+  const {value} = req.body;
+  const id = +req.params.id;
+
+  const todo = await todoRepository.findOneByOrFail({id});
+
+  todo.value = value;
+  const updatedTodo = await todoRepository.save(todo)
+
+  res.status(200).send(updatedTodo);
 })
